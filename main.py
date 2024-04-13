fda6d9f2b9a571daabc1348bd1ade3b419658949
Saku Mizuhara <sakuyakonohana39@gmail.com>
2024-04-13 17:52:04 +0900

    handle to emptypage error

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 discord_client = discord.Client(intents=intents)
 arxiv_client = arxiv.Client()
+to_process = set()
 
 
 @discord_client.event
@@ -29,37 +30,43 @@ async def on_ready():
 @tasks.loop(seconds=60)
 async def loop():
     await discord_client.wait_until_ready()
+    global to_process
     dt_now = datetime.datetime.now()
-    if dt_now.hour != 6 or dt_now.minute != 0:
-        return
+    if dt_now.hour == 6 and dt_now.minute == 0:
+        for guild in discord_client.guilds:
+            for channel in guild.text_channels:
+                if channel.name[:4] == "auto":
+                    to_process.add(channel)
     since = dt_now - datetime.timedelta(days=2)
     until = dt_now - datetime.timedelta(days=1)
-    for guild in discord_client.guilds:
-        for channel in guild.text_channels:
-            if channel.name[:4] == "auto":
-                query = parse(channel.topic)
-                append_string = 'submittedDate:[{} TO {}]'.format(
-                    datetime.datetime.strftime(since, '%Y%m%d%H%M%S'), datetime.datetime.strftime(until, '%Y%m%d%H%M%S'))
-                if len(query.query) == 0:
-                    query.query = append_string
-                else:
-                    query.query += ' AND ' + append_string
-                query.max_results = None
-                result = arxiv_client.results(query)
-                return_list = [""]
-                for r in result:
-                    next_content = r.title + '\n<' + str(r) + '>\n'
-                    if len(return_list[-1]) + len(next_content) > 2000:
-                        return_list.append(next_content)
-                    else:
-                        return_list[-1] += next_content
-                sent = False
-                for ret in return_list:
-                    if len(ret) > 0:
-                        await channel.send(ret[:-1])  # remove last \n
-                        sent = True
-                if sent == False:
-                    await channel.send('No results found')
+    processed = set()
+    for channel in to_process:
+        query = parse(channel.topic)
+        append_string = 'submittedDate:[{} TO {}]'.format(
+            datetime.datetime.strftime(since, '%Y%m%d%H%M%S'), datetime.datetime.strftime(until, '%Y%m%d%H%M%S'))
+        if len(query.query) == 0:
+            query.query = append_string
+        else:
+            query.query += ' AND ' + append_string
+        query.max_results = None
+        result = arxiv_client.results(query)
+        return_list = [""]
+        for r in result:
+            next_content = r.title + '\n<' + str(r) + '>\n'
+            if len(return_list[-1]) + len(next_content) > 2000:
+                return_list.append(next_content)
+            else:
+                return_list[-1] += next_content
+        processed.add(channel)
+        sent = False
+        for ret in return_list:
+            if len(ret) > 0:
+                await channel.send(ret[:-1])  # remove last \n
+                sent = True
+        if not sent:
+            await channel.send('No results found')
+    for channel in processed:
+        to_process.remove(channel)
 
 
 @discord_client.event
